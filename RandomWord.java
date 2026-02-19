a421f26b86ec6bf91bbd04b69e77d3ede7d48883
Vlad75518 <7551876@stud.kai.edu.ua>
2026-02-20 00:07:23 +0200

    Add files via upload

---
+++ RandomWord.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = null;
+        int count = 0;
+
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            count++;
+
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champion = word;
+            }
+        }
+
+        if (champion != null) {
+            System.out.println(champion);
+        }
+    }
+}
